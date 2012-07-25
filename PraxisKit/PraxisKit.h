373c6721c3454abb232e9a99e7be788559a0e8e6
Hilario Pérez Corona <hpcorona@gmail.com>
2012-07-25 00:25:06 -0700

    Initial Commit

---
+++ PraxisKit/PraxisKit.h
@@ -0,0 +1,13 @@
+//
+//  PraxisKit.h
+//  PraxisKit
+//
+//  Created by Hilario Perez Corona on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PraxisKit : NSObject
+
+@end
